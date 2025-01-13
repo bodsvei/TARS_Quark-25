8ab2c3c3714caa64c56c548916c30dcd45f9640d
Anirudh Singh Air <83878255+AnirudhSPACEACE@users.noreply.github.com>
2025-01-13 20:30:01 +0530

    hope it works

--- main.py
+++ main.py
@@ -9,19 +9,20 @@ def recordText():
             with sr.Microphone() as source2:
                 r.adjust_for_ambient_noise(source2, duration=0.2)
                 audio2 = r.listen(source2)
-                text = r.recognize_google(audio2)
+                _text = r.recognize_google(audio2)
 
-                return text
+                return _text
 
         except sr.RequestError as e:
             print("Could not request results:{0}".format(e))
-        
-        
 
-    return text
+        except:
+            print("Unknown errors encountered")
+
+    return _text
 
 
-def writeText():
+def writeText(text):
     f = open("out.txt", "a")
     f.write(text)
     f.write("/n")
@@ -32,6 +33,7 @@ def writeText():
 
 while(1):
     text = recordText()
+    print(text)
     out = writeText(text)
 
     print("FILE CREATED")
