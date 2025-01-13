2b34879c6a925e38ea724cf54af7f0a0eb288c31
Anirudh Singh Air <83878255+AnirudhSPACEACE@users.noreply.github.com>
2025-01-13 19:57:35 +0530

    Working on Speech to Text

---
+++ main.py
@@ -0,0 +1,33 @@
+import speech_recognistion as sr
+import pyttsx3
+
+r=sr.Recognizer()
+
+def recordText():
+    while(1):
+        try:
+            with sr.Microphone() as source2:
+                r.adjust_for_ambient_noise(source2, duration=0.2)
+                audio2 = r.listen(source2)
+                text = r.recognize_google(audio2)
+
+                return text
+
+        except sr.RequestError as e:
+            print("Could not request results: {0}".format{e})
+        
+        except:
+            print("Unknown error encountered.")
+
+    return
+
+
+def writeText():
+    return
+
+
+while(1):
+    text = recordText()
+    out = writeText(text)
+
+    print("FILE CREATED")
