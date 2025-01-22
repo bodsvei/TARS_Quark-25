b2de4b73bf48b8e8af9573e8a5ddf33db1ecd6e0
Anirudh Singh Air <83878255+bodsvei@users.noreply.github.com>
2025-01-22 15:33:03 +0530

    Update Text to speech.py

--- Text to speech.py
+++ Text to speech.py
@@ -1,12 +1,6 @@
 import pyttsx3
 
 def speak(text):
-  """
-  Converts the given text to speech using pyttsx3.
-
-  Args:
-    text: The text to be spoken.
-  """
   engine = pyttsx3.init() 
   engine.say(text)
   engine.runAndWait() 
