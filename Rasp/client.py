97fc4ea1132f0e32ac3638240753e92b8634e19d
Anirudh Singh Air <83878255+bodsvei@users.noreply.github.com>
2025-02-06 14:21:30 +0530

    Update client.py

--- Rasp/client.py
+++ Rasp/client.py
@@ -1,13 +1,38 @@
 import socket
+from os import _exit
+from threading import Thread
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-print ("Socket successfully created")
 
-IP="0.0.0.0"
-port = 8000
+class Client:
+
+    def __init__(self, HOST, PORT):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect(HOST, PORT)
+
+        self.TalkToServer()
+
+    def TalkToServer(self):
+        Thread(target=self.recieveMessage).start()
+        self.sendMessage()
+    
+    def sendMessage(self, client_socket):
+        while True:
+            clientMessage=input(" ")
+            client_socket.send(clientMessage.encode())
+
+    def recieveMessage(self):
+        while True:
+            sMsg = self.socket.recv(1024).decode()
+            if (sMsg == "bye" or not sMsg.strip()):
+                _exit(0)
+            print("\033[1;31;40m" + "TARS: " + sMsg + "\033[0m")
+
+if __name__ == "__main__":
+    Client
 
 s.connect((IP, port))
 
 def requestLLMOut():
     msg=s.recv(8)
     return msg
+
